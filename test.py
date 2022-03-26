14601b35c82c14197f9bdf3b7a4a9ff9c0816fec
Christopher <ctran457@gmail.com>
2022-03-26 17:12:55 -0500

    Fixed test.py code bc I'm dumb and didn't test test.py

--- test.py
+++ test.py
@@ -13,10 +13,9 @@ NROWS = 50
 COLS = ["ARTIST_NAME", "ARTIST_URL", "SONG_NAME", "SONG_URL", "LYRICS"]
 
 
-def create_all_data(_path):
-    if os.path.exists(_path):
-        # If file "_path" already exists, return False
-        return False
+def create_all_data(_path, _filename):
+    if os.path.exists(os.path.join(_path, _filename)):
+        os.remove(os.path.join(_path, _filename))
 
     data = {c: [] for c in COLS}
 
@@ -34,27 +33,28 @@ def create_all_data(_path):
                     data[COLS[4]].append(" ".join(row[4:]))  # LYRICS
 
     df = pd.DataFrame(data)
-    df.to_csv(_path, index=False)
+    df.to_csv(os.path.join(_path, _filename), index=False)
 
     # If file "_path" was created, return True
     return True
 
 
-def read_all_data(_path, _nrows=None):
-    if not os.path.exists(_path):
-        create_all_data()
+def read_all_data(_path, _filename, _nrows=None):
+    if not os.path.exists(os.path.join(_path, _filename)):
+        create_all_data(_path, _filename)
 
-    df = pd.read_csv(os.path.join(_path), nrows=_nrows)
+    df = pd.read_csv(os.path.join(_path, _filename), nrows=_nrows)
 
     return df
 
 
 def __main__():
     # Path to location of all_data.csv file
-    path = os.path.join(os.getcwd(), "data", "azlyrics-csv", "all_data.csv")
+    path = os.path.join(os.getcwd(), "data", "azlyrics-csv")
+    filename = "all_data.csv"
 
     # Read all data into a pandas dataframe
-    all_data_df = read_all_data(path, NROWS)
+    all_data_df = read_all_data(path, filename, NROWS)
 
     # Create corpus with lyrics, tokenize lyrics before adding to corpus.
     corpus = [
