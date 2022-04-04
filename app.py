21d78d9cde539289c371373c57199c1f6ddc5e18
Rose Soriano <rosesoriano.dtx@gmail.com>
2022-04-04 12:23:18 -0500

    Made changes in order to get query POST

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-
 @app.route('/find', methods=['GET', 'POST'])
 def find():
+    if request.method == "POST":
+        user_query = request.form["query"].lower()  # You will see name="query" at our form's input on find.html
     return render_template('find.html')
